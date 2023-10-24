95715fe1664dafcc421bd6ae3e88c2979205c5ab
Matthew Molloy <molloyin@icloud.com>
2023-10-24 22:06:31 +1300

    feature: added masked multi head attention module

---
+++ src/scratchmodel.py
@@ -0,0 +1,30 @@
+"""
+Author:         Matthew Molloy
+Date:           14/10/2023
+Description:    Hyperparams, training loop, and evaluation logic for AI ./model.py
+"""
+
+
+# masked multihead attention
+    #scaled dot-product
+    # split heads
+    # merge heads
+
+# feed forward
+
+# positional encoding
+
+# add and norm
+
+# linear
+
+# softmax 
+
+## decoder only transformer
+    #positonal encoding
+    # masked multi-head
+    # add and norm
+    # feed forward
+    # add and norm
+    # linear
+    # softmax
